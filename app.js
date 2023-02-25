d2632ed1aaabeaac4b24b25ab77ae03160bd0cda
Lee Chuan <119503248+leeechuan@users.noreply.github.com>
2023-02-25 22:53:29 +0800

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var submitButton = document.getElementById("submit_form");
-    var form = document.getElementById("email_form");
-    form.addEventListener("submit", function (e) {
-        setTimeout(function() {
-            submitButton.value = "Sending...";
-            submitButton.disabled = true;
-        }, 1);
-    });
